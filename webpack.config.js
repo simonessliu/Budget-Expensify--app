ffbfcbbad646ec8994174be7236d8afeea1b8f7a
simon <simonlyllyyy@gmail.com>
2019-07-30 12:33:07 +1000

    Setup production webpack build

--- webpack.config.js
+++ webpack.config.js
@@ -1,34 +1,57 @@
 //entry -> output 
 
 const path = require('path');
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 
-module.exports = {
-    entry : './src/app.js',
-    output : {
-        path: path.join(__dirname,'public'),
-        filename: 'bundle.js'
-    },
+module.exports = (env) =>{
+    const isProduction = env === 'production';
 
-    module: {
-        rules: [{
-            loader : 'babel-loader',
-            test : /\.js$/,
-            exclude: /node_modules/
-        },{
-            test:/\.s?css$/,
-            use: [
-                'style-loader',
-                'css-loader',
-                'sass-loader'
-            ]
-        }]
-    },
-    devtool : 'cheap-module-eval-source-map', //devtool can help find us where the error is exactly
+    return {
+        entry : './src/app.js',
+        output : {
+            path: path.join(__dirname,'public'),
+            filename: 'bundle.js'
+        },
     
-    devServer : {
-        contentBase : path.join(__dirname,'public'),
-        historyApiFallback:true // tell server that we are gonna handle routing via our client side code and it should return this page for all 404 routes 
+        module: {
+            rules: [{
+                loader : 'babel-loader',
+                test : /\.js$/,
+                exclude: /node_modules/
+            },{
+                test:/\.s?css$/,
+                use: [
+                    MiniCssExtractPlugin.loader,
+                    {
+                        loader:'css-loader',
+                        options:{
+                            sourceMap:true
+                        }
+                    },
+                    {
+                        loader:'sass-loader',
+                        options:{
+                            sourceMap:true
+                        }
+                    }
+                   
+                ]
+            }]
+        },
+        plugins: [
+            new MiniCssExtractPlugin({
+                filename:'styles.css'
+            })
+        ],
+        devtool : isProduction ? 'source-map':'inline-source-map', //devtool can help find us where the error is exactly
+        
+        devServer : {
+            contentBase : path.join(__dirname,'public'),
+            historyApiFallback:true // tell server that we are gonna handle routing via our client side code and it should return this page for all 404 routes 
+        }
     }
-};
+}
+
+
 
 
